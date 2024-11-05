8bbe5f99946ae179e2b7170ed156138596eeeb6a
wenyan <sabertobihwy@gmail.com>
2024-11-04 19:55:51 -0500

    webpack

---
+++ nodejs/webpack/webpack-proj/src/index.js
@@ -0,0 +1,10 @@
+import  a from "./a"
+import  b from "./b"
+import "./style.less"
+import $ from "jquery"
+import url from './a.png'
+
+console.log(url)
+$('#app').text("1234") 
+//$('<h1>').text("123").appendTo(document.body) 
+console.log(a,b)
